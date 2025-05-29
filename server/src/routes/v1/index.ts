21f99579f156bcf3e3362a0978e7d35e09124299
Manish <[manishbulchandani4@gmail.com]>
2025-05-29 16:59:53 +0530

    server build fix

--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -1,15 +1,15 @@
 import express from "express";
-import userRoutes from "../../modules/user/route/user.routes";
-import adminRoutes from "../../modules/admin/route/admin.route";
-import questionRoutes from "../../modules/questions/route/question.routes";
+// import userRoutes from "../../modules/user/route/user.routes";
+// import adminRoutes from "../../modules/admin/route/admin.route";
+// import questionRoutes from "../../modules/questions/route/question.routes";
 
 
 const router = express.Router();
 
-router.use("/user", userRoutes);
-router.use("/admin", adminRoutes);
-router.use("/question", questionRoutes);
-router.use("/infoCard", questionRoutes);
+// router.use("/user", userRoutes);
+// router.use("/admin", adminRoutes);
+// router.use("/question", questionRoutes);
+// router.use("/infoCard", questionRoutes);
 
 
 
